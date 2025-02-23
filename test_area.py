cbae67ccdc0e0cf8057ff7f2fe6322f54353fc21
Adiykarr <adiykar2001@gmail.com>
2025-02-23 22:36:58 +0530

    2nd rocket

--- test_area.py
+++ test_area.py
@@ -1,14 +1,9 @@
-import pytest  
+import pytest
 from area import calculate_area_square  
-  
-def test_calculate_area_square_negative():  
-    with pytest.raises(TypeError):  
-        calculate_area_square(-2)  
-  
-def test_calculate_area_square_string():  
-    with pytest.raises(TypeError):  
-        calculate_area_square("2")  
-  
-def test_calculate_area_square_list():  
-    with pytest.raises(TypeError):  
-        calculate_area_square([2])
+
+def test_calculate_area_square_valid():
+    assert calculate_area_square(5) == 25
+
+def test_calculate_area_square_negative():
+    with pytest.raises(TypeError):
+        calculate_area_square(-2)
